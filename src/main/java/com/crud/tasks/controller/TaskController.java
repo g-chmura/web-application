0c8cb6bc708ae4a72f544f07c15f5297fa6e0675
g-chmura <gchmura98@gmail.com>
2019-12-16 14:58:59 +0100

    REST API part I

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+
+    @RequestMapping(method = RequestMethod.GET, value ="getTasks")
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value ="getTask")
+    public TaskDto getTask() {
+        return new TaskDto(1L, "Test", "test_content");
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+    public void deleteTask (){
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTask (){
+        return new TaskDto(1L, "Edited test", "Test content");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public void createTask (){
+    }
+
+}
